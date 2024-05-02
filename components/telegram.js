7bfbbc6bb114ebe4cb1bbf1d0e4dd9fbbe8c7b53
Favour Kelvin <favourkelvin17@gmail.com>
2024-05-02 23:53:59 +0100

    fix seo issue in docs

---
+++ components/telegram.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TelegramIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+    <path d="M12 0C5.37 0 0 5.37 0 12s5.37 12 12 12 12-5.37 12-12S18.63 0 12 0zm5.89 9.75l-1.88 8.83c-.14.64-.52 1.04-1.17 1.04-.4 0-.72-.16-.99-.4l-2.95-2.21-1.42 1.36c-.16.16-.28.28-.52.28-.26 0-.4-.12-.4-.41l-.01-2.78-5.18-1.57c-.54-.17-.55-.54.12-.8l20.27-7.78c.38-.13.76.09.64.71z"/>
+  </svg>
+);
+
+export default TelegramIcon;
