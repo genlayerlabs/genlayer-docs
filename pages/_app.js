934930d8c719b44150085f47cf96de359641fd60
Cristiam Da Silva <cristiam86@gmail.com>
2025-06-12 11:36:22 +0200

    refactor: updated logo, font, and colors

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 // pages/_app.js
 import "/pages/style.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import localFont from 'next/font/local'
+
+const myFont = localFont({
+  src: './fonts/switzer-regular.woff2',
+})
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        body {
+          font-family: ${myFont.style.fontFamily};
+        }
+      `}</style>
       <GoogleAnalytics trackPageViews gaMeasurementId="G-K1DWDNGBKV" />
       <Component {...pageProps} />
     </>
