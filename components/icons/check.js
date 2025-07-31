c447d3c6f9a9549957331bf33c4dc4fb2a69202a
Edinaldo Junior <epsjunior@icloud.com>
2025-07-30 21:47:21 -0300

    chore: new necessary icons

---
+++ components/icons/check.js
@@ -0,0 +1,7 @@
+export default function CheckIcon({ className }) {
+  return (
+    <svg className={className} width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+    </svg>
+  );
+} 
